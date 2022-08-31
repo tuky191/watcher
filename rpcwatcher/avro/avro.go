a8ab5a5d964862620f87dc8ae44fc2bf2b00e582
Michal Turcan <michal.turcan.za@gmail.com>
2022-08-31 18:27:16 +0200

    sync

--- rpcwatcher/avro/avro.go
+++ rpcwatcher/avro/avro.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"reflect"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/rs/zerolog/log"
 )
 
@@ -26,9 +25,10 @@ type AvroField struct {
 
 func GenerateAvroSchema(model interface{}) string {
 	fmt.Printf("%#v\n", model)
-	st, err := json.Marshal(getAvroRecords(reflect.TypeOf(model)))
+	record := getAvroRecords(reflect.TypeOf(model))
+	st, err := json.Marshal(record)
 	var prettyJSON bytes.Buffer
-	error := json.Indent(&prettyJSON, st, "", "\t")
+	error := json.Indent(&prettyJSON, st, "", " ")
 	if err != nil {
 		log.Err(err).Msg("")
 		return ""
@@ -39,44 +39,35 @@ func GenerateAvroSchema(model interface{}) string {
 	return prettyJSON.String()
 }
 
-func getAvroRecords(model reflect.Type) []AvroSchema {
-
+func getAvroRecords(model reflect.Type) AvroSchema {
 	val := strctTyp(model)
-	spew.Dump(val)
+
 	record := AvroSchema{
 		Type:      "record",
 		Name:      val.Name(),
 		Namespace: "some.namespace",
 		Fields:    getAvroFields(val),
 	}
-	AvroSchemas = append(AvroSchemas, record)
-	return AvroSchemas
+
+	return record
 }
 func getAvroFields(model reflect.Type) []AvroField {
 	fields := make([]AvroField, 0, 10)
 
 	val := strctTyp(model)
-	//spew.Dump(val)
-
-	if val.Kind() != reflect.Struct {
-		field := AvroField{
-			Name: val.Name(),
-			Type: val.Kind().String(),
-		}
-		fields = append(fields, field)
-		return fields
-	}
 	for i := 0; i < val.NumField(); i++ {
 		t := val.Field(i)
 
 		if val.Field(i).Type.Kind() == reflect.Struct || val.Field(i).Type.Kind() == reflect.Ptr {
+
+			// switch val.Field(i).Type.Kind() {
+
+			// }
 			field := AvroField{
 				Name: t.Name,
-				Type: val.Field(i).Type.Kind().String(),
+				Type: getAvroRecords(val.Field(i).Type),
 			}
 			fields = append(fields, field)
-			//Skip private/non exportable fields
-			_ = getAvroRecords(val.Field(i).Type)
 			continue
 		}
 
@@ -92,26 +83,7 @@ func getAvroFields(model reflect.Type) []AvroField {
 func strctTyp(s reflect.Type) reflect.Type {
 	if s.Kind() == reflect.Ptr {
 		s = s.Elem()
-		spew.Dump(s)
 	}
-	//spew.Dump(t)
-	//n := reflect.TypeOf(s).Elem().Name()
-	//fmt.Println(n)
-	// if pointer get the value
-	for i := 0; i < s.NumField(); i++ {
-		f := s.Field(i)
-		spew.Dump(f)
-	}
-
-	// for v.Kind() == reflect.Ptr {
-
-	// 	spew.Dump(v.Elem())
-
-	// 	v = reflect.Indirect(reflect.ValueOf(s))
-	// 	// spew.Dump(reflect.ValueOf(&s))
-	// 	// spew.Dump(reflect.Indirect(reflect.ValueOf(&s)))
-	// 	// spew.Dump(reflect.New(v.Type().Elem()))
-	// }
 
 	return s
 }
