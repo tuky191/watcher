650e88590c2098bdb63e1bdf7e2b2d810a2545b9
tuky191 <michal@terra.money>
2022-09-02 15:53:19 +0200

    sync

--- cmd/consumer/consumer.go
+++ cmd/consumer/consumer.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"rpc_watcher/rpcwatcher/avro"
 
 	"github.com/apache/pulsar-client-go/pulsar"
+	"github.com/terra-money/mantlemint/block_feed"
 )
 
 func main() {
@@ -13,16 +15,27 @@ func main() {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL: "pulsar://localhost:6650",
 	})
-
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Close()
+	b := &block_feed.BlockResult{}
+	schema := avro.GenerateAvroSchema(&block_feed.BlockResult{})
+
+	consumerJS := pulsar.NewJSONSchema(schema, nil)
 
 	chainName := "localterra"
 	eventKind := "tm.event='NewBlock'"
 	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
-		Topic:            "persistent://terra/" + chainName + "/" + eventKind,
-		SubscriptionName: "my-sub1",
-		Type:             pulsar.Exclusive,
+		Topic:                       "persistent://terra/" + chainName + "/" + eventKind,
+		SubscriptionName:            "my-sub1",
+		Type:                        pulsar.Exclusive,
+		Schema:                      consumerJS,
+		SubscriptionInitialPosition: pulsar.SubscriptionPositionLatest,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	defer consumer.Close()
 
@@ -30,7 +43,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = msg.GetSchemaValue(&b)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
-		msg.ID(), string(msg.Payload()))
+	fmt.Printf("Received message msgId: %#v -- content:\n '%+v'\n",
+		msg.ID(), b)
 }
