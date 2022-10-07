6bbeb43d7befd01a997b70f3bb9acb8a19657997
Michal Turcan <michal.turcan.za@gmail.com>
2022-10-07 15:51:08 +0200

    fixes and refactoring in decodetx pulsar go function

--- cmd/decodetx/main.go
+++ cmd/decodetx/main.go
@@ -3,34 +3,30 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"rpc_watcher/rpcwatcher/avro"
-	"rpc_watcher/rpcwatcher/logging"
 	"strconv"
-	"time"
 
 	log "github.com/apache/pulsar/pulsar-function-go/logutil"
-
-	pulsar_types "rpc_watcher/rpcwatcher/helper/types/pulsar"
-	pulsar_producer "rpc_watcher/rpcwatcher/pulsar"
 
 	rpc_types "rpc_watcher/rpcwatcher/helper/types/rpc"
 
 	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/apache/pulsar/pulsar-function-go/pf"
 	abci "github.com/tendermint/tendermint/abci/types"
-	tmjson "github.com/tendermint/tendermint/libs/json"
 
 	"rpc_watcher/rpcwatcher/helper/rpc"
 )
 
 var topic string = ""
 var schema, _ = avro.GenerateAvroSchema(&rpc_types.TxRecord{})
-var p pulsar_types.Producer
+var jsonSchemaWithProperties *pulsar.JSONSchema
+var properties = make(map[string]string)
+var producer pulsar.Producer
 
 func PublishFunc(ctx context.Context, in []byte) error {
 	fctx, ok := pf.FromContext(ctx)
 	tx := &abci.TxResult{}
-
 	if !ok {
 		return errors.New("get Go Functions Context error")
 	}
@@ -44,39 +40,27 @@ func PublishFunc(ctx context.Context, in []byte) error {
 	decodedTx, err := rpc.DecodeTx(*tx)
 
 	//Temp fix for txs that were retrieved via websocket and didnt have event_time set. Already rectified by setting the event_time from the block timestamp value.
-	if !record.EventTime().IsZero() {
+	if record.EventTime().Unix() > 0 {
 		decodedTx.Timestamp = record.EventTime()
+		log.Infof("Timestamp set to message EventTime: %s", record.EventTime().String())
 	} else {
 		decodedTx.Timestamp = record.PublishTime()
-
+		log.Infof("EventTime not set, Timestamp set to message PublishTime: %s", record.PublishTime().String())
 	}
 
 	if err != nil {
 		log.Fatalf("Unable to decode source tx:", "tx", record.ID(), "error:", err)
 	}
 
-	decodedTxjson, err := tmjson.Marshal(decodedTx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Infof("%s", decodedTxjson)
+
 	if topic == "" {
-		topic = "persistent://" + fctx.GetTenantAndNamespace() + "/" + "txdecoded"
-		properties := make(map[string]string)
+		topic = fctx.GetOutputTopic()
 		properties["pulsar"] = "EHLO"
-		jsonSchemaWithProperties := pulsar.NewJSONSchema(schema, properties)
-		o := pulsar_types.PulsarOptions{
-			ClientOptions: pulsar.ClientOptions{
-				URL:               "pulsar://proxy1:6650",
-				OperationTimeout:  30 * time.Second,
-				ConnectionTimeout: 30 * time.Second,
-			},
-			ProducerOptions: pulsar.ProducerOptions{Topic: topic, Schema: jsonSchemaWithProperties},
-		}
-		p, err = pulsar_producer.NewProducer(&o)
-		if err != nil {
-			log.Fatalf("Unable to start producer", "error:", err)
-		}
+		jsonSchemaWithProperties = pulsar.NewJSONSchema(schema, properties)
+		producer = fctx.NewOutputMessage(fctx.GetOutputTopic())
 	}
 
 	message := pulsar.ProducerMessage{
@@ -84,13 +68,16 @@ func PublishFunc(ctx context.Context, in []byte) error {
 		SequenceID:  &decodedTx.Height,
 		OrderingKey: strconv.FormatInt(decodedTx.Height, 10),
 		EventTime:   decodedTx.Timestamp,
+		Schema:      jsonSchemaWithProperties,
 	}
-	l := logging.New(logging.LoggingConfig{
-		Debug: true,
-		JSON:  true,
-	})
 
-	p.SendMessage(l, message)
+	msg, err := producer.Send(ctx, &message)
+	if err != nil {
+		log.Fatalf("Unable to send message", "error:", err)
+	}
+
+	log.Infof("Submitted tx: %s at height: %s with timestamp: %s as (%s,%s,%s)", decodedTx.TxHash, fmt.Sprint(decodedTx.Height), decodedTx.Timestamp.String(), fmt.Sprint(msg.LedgerID()), fmt.Sprint(msg.EntryID(), fmt.Sprint(msg.PartitionIdx())))
+
 	return nil
 }
 
